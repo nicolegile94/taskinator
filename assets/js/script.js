1603c92a95637fd4b981494122dba0ddf7d03cc1
Nicole Gile <nicolegile94@gmail.com>
2020-06-21 16:10:33 -0500

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var taskInProgressEl = document.querySelector("#task-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 var formEL = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
@@ -16,13 +18,39 @@ var taskFormHandler = function(event){
 
     formEL.reset();
 
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
+    var isEdit = formEL.hasAttribute("data-task-id");
+
+    //has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId =formEL.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
     }
 
+    //no data attribute, so create as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+    
     createTaskEl(taskDataObj);
-    };
+    }
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+//find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+//set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEL.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
+};
+
 
 var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
@@ -126,4 +154,29 @@ var editTask = function(taskId) {
     formEL.setAttribute("data-task-id", taskId);
 }
 
+var taskStatusChangeHandler = function(event) {
+    //get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    //find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+
+    else if (statusValue === "in progress") {
+        taskInProgressEl.appendChild(taskSelected);
+    }
+
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
